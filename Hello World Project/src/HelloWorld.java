323c7e5849224209f03bd7e0c2f3bbf2c798cece
APCS2nd <APCS2nd@MHS-LT9-08>
2019-08-21 12:17:02 -0700

    Complete Hello World project

---
+++ Hello World Project/src/HelloWorld.java
@@ -0,0 +1,13 @@
+//Robert Jian 
+//August 16, 2019
+//First program in java for APCS
+
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+
+	}
+
+}
