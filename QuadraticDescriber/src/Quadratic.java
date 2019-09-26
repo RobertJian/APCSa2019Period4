a6af9fd0c3bc99d1579f80d2f72ad681ebb536f6
RobertJian <20jianr01@stu.smuhsd.org>
2019-09-25 23:45:15 -0700

    Quadratic added

---
+++ QuadraticDescriber/src/Quadratic.java
@@ -0,0 +1,8 @@
+/*Robert Jian
+ * 4th Period
+ * September 29, 2019
+ * a class responsible for the all the calculations.
+ */
+public class Quadratic {
+
+}
---
+++ QuadraticDescriber/src/QuadraticClient.java
@@ -0,0 +1,13 @@
+/*Robert Jian
+ * 4th Period
+ * September 25, 2019
+ * contain a main method and handle interactions with the user. 
+ */
+public class QuadraticClient {
+
+	public static void main(String[] args) {
+	
+
+	}
+
+}
