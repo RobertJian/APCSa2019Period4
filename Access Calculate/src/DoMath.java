d08f72cb0f8e978a88f98a16164fdcdd9295a0b8
RobertJian <20jianr01@stu.smuhsd.org>
2019-09-27 22:00:28 -0700

    update changes

--- Access Calculate/src/DoMath.java
+++ Access Calculate/src/DoMath.java
@@ -7,14 +7,14 @@ public class DoMath {
 
 	public static void main(String[] args) {
 	//Part 1
-		System.out.println(Calculate.square(-11)); 
-		System.out.println(Calculate.cube(-2));
-		System.out.println(Calculate.average(3, 50));
+		System.out.println(Calculate.square(-9)); 
+		System.out.println(Calculate.cube(-3));
+		System.out.println(Calculate.average(66, 50));
 		System.out.println(Calculate.average(30.4, 82, 94));
 		System.out.println(Calculate.toDegrees(3.14159));
 		System.out.println(Calculate.toRadians(360));
 		System.out.println(Calculate.discriminant(3, 8, 4));
-		System.out.println(Calculate.toImproperFrac(3, 1, 5));
+		System.out.println(Calculate.toImproperFrac(3, 1, 9));
 		System.out.println(Calculate.toMixedNum(8, 5));
 		System.out.println(Calculate.foil(2, 2 ,2, 2, "x"));
 	//Part 2
