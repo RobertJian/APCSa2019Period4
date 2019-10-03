b5480d22745c3ac637496cf728393a559cf11910
APCS2nd <APCS2nd@MHS-LT9-08>
2019-10-03 10:59:53 -0700

    changes made on Pair Program

--- Pair Programming/src/ProcessingNumbers.java
+++ Pair Programming/src/ProcessingNumbers.java
@@ -6,32 +6,35 @@ public class ProcessingNumbers {
 		Scanner userInput = new Scanner(System.in);
 		System.out.print("How many times do you want to run your numbers? : ");
 		int times = userInput.nextInt();
-		int max; 
-		int min;
+		System.out.print("what is your first value :");
+		int value = userInput.nextInt();
+		int max = value; 
+		int min = value;
 		int evenMax = 0;
-		int sum = 0;
-		for(int i = 1; i <= times; i++) {
-			System.out.print("What is your value? : ");
-			int value = userInput.nextInt();
-			max =value;
-			min =value;
-			if(value>max) {
-				max = value;
-			}else if(value<min){
-				min = value;
+		int evenSum = 0;
+		if(value%2 == 0) {
+			evenSum = value;
+			evenMax = value;
+		}
+		for(int i = 1; i < times; i++) {
+			System.out.print("What is your next value? : ");
+			int scanValue = userInput.nextInt();
+			if(scanValue>max) {
+				max = scanValue;
+			}else if(scanValue<min){
+				min = scanValue;
 			}
-			if(value%2 ==0) {
-				sum += value;
-				if(value>evenMax) {
-					evenMax = value;
+			if(scanValue%2 ==0) {
+				evenSum += scanValue;
+				if(scanValue>evenMax) {
+					evenMax = scanValue;
 				}
 			}
-			System.out.print(value + " ");
 			
 		}
 		System.out.println("Biggest number is: " + max);
 		System.out.println("Smallest number is: " + min);
-		System.out.println("The sum of all even number is " + sum + "and the biggest even number is " + evenMax );
+		System.out.println("The sum of all even number is " + evenSum + " and the biggest even number is " + evenMax );
 		userInput.close();
 	}
 
