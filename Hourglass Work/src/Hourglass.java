2c1567b59c2293dc6b6a4576d590b4fffb888bf3
APCS2nd <APCS2nd@MHS-LT9-08>
2019-10-09 13:08:01 -0700

    hourglass

---
+++ Hourglass Work/src/Hourglass.java
@@ -0,0 +1,41 @@
+/*Robert Jian
+ * October 9, 2019
+ * 4th
+ * to understand the usage of nested loops
+ */
+import java.util.*;
+
+public class Hourglass {
+	
+	public static void line() {
+		System.out.print("|");
+	}
+	public static void quote() {
+		System.out.print("\"");
+	}
+	public static void backslash() {
+		System.out.print("\\");
+	}
+	public static void slash() {
+		System.out.print("/");
+	}
+	public static void colon() {
+		System.out.print(":");
+	}
+	public static void space() {
+		System.out.print(" ");
+	}
+	/*public static void main(String[] args) {
+		Scanner ourGlass = new Scanner (System.in);
+		System.out.println("What is the size of your hourglass?: ");
+		int size = ourGlass.nextInt();
+		line();
+		for(int quote = 1; quote <= size; quote++) {
+			quote();
+		}
+		line();
+		for(int space = 1; )
+	}
+*/
+
+}
