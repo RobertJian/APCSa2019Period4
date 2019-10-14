216c7868f7166208a8fd87940f3b13c35e52994d
APCS2nd <APCS2nd@MHS-LT9-08>
2019-10-14 11:54:37 -0700

    fixed

--- Pair Programming/src/ProcessingNumbers.java
+++ Pair Programming/src/ProcessingNumbers.java
@@ -10,13 +10,10 @@ public class ProcessingNumbers {
 		int value = userInput.nextInt();
 		int max = value; 
 		int min = value;
-		int evenMax = -2147483648;
+		int evenMax = value;
 		int evenSum = 0;
-		if(value%2!=0) {
-			evenMax = 0;
-		}else{
+		if(value%2==0) {
 			evenSum = value;
-			evenMax = value;
 		}
 		for(int i = 2; i <= times; i++) {
 			System.out.print("What is your value " + i + "?: ");
@@ -27,19 +24,25 @@ public class ProcessingNumbers {
 			}else if(scanValue<min){
 				min = scanValue;
 			}
-			if(scanValue%2 ==0) {
+			if(value%2!=0 && scanValue%2 == 0) {
+				evenMax = scanValue;
+				evenSum = scanValue;
+			}else if(scanValue%2 ==0) {
 				evenSum += scanValue;
 				if(scanValue>evenMax) {
 					evenMax = scanValue;
 				}
 			}
-			
 		}
 		System.out.println("The maximum is: " + max);
 		System.out.println("the minimum is: " + min);
 		System.out.println("The sum of all even number is " + evenSum);
-		if(evenMax%2==0 && value !=0) {
+		if(evenMax%2==0 ) {
 		System.out.println("The maximum even number is " + evenMax );
+		}else if(evenMax == 0){
+			System.out.println("The maximum even number is 0.");
+		}else {
+			System.out.println("There is no even max.");
 		}
 		userInput.close();
 	}
